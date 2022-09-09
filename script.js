92e5c1cf526da0038ca1fffb45335af629e47e49
chubi-x <chubixax@protonmail.com>
2022-09-09 12:38:52 +0100

    feat: testimonial slider v2

--- script.js
+++ script.js
@@ -5,49 +5,78 @@ const cards = document.querySelectorAll(".testimonial-card");
 const prevBtn = document.querySelector(".prev");
 const nextBtn = document.querySelector(".next");
 
-function slideControl(control, cards) {
-  for (let i = 0; i < cards.length; i++) {
-    if (control === "prev" && cards[i].classList.contains("active")) {
-      if (i - 1 >= 0) {
-        toggleClass(cards[i], cards[i - 1]);
-        break;
-      } else {
-        toggleClass(cards[i], cards[cards.length - 1]);
-        break;
-      }
-    } else if (control === "next" && cards[i].classList.contains("active")) {
-      if (i + 1 < cards.length) {
-        toggleClass(cards[i], cards[i + 1], "next");
-        break;
-      } else {
-        toggleClass(cards[i], cards[0], "next");
-        break;
-      }
-    } else {
-      console.log("invalid control");
-    }
-  }
+let currSlide = 0;
+let maxSlides = cards.length - 1;
+
+function moveSlide(slides, currSlide) {
+  slides.forEach(
+    (slide, index) =>
+      (slide.style.transform = `translateX(${(index - currSlide) * 100}%)`)
+  );
 }
 
-function toggleClass(present, next, className) {
-  present.classList.remove("active");
-  next.classList.add("active", className);
-}
+prevBtn.addEventListener("click", () => {
+  if (currSlide === 0) {
+    currSlide = maxSlides;
+  } else currSlide--;
 
-prevBtn.addEventListener("click", () => slideControl("prev", cards));
-nextBtn.addEventListener("click", () => slideControl("next", cards));
+  moveSlide(cards, currSlide);
+});
+
+nextBtn.addEventListener("click", () => {
+  if (currSlide === maxSlides) {
+    currSlide = 0;
+  } else currSlide++;
+  moveSlide(cards, currSlide);
+});
+
+moveSlide(cards, currSlide);
+
+//#########################
+// OLD SLIDER LOGIC
+//#########################
+
+// function slideControl(control, cards) {
+//   for (let i = 0; i < cards.length; i++) {
+//     if (control === "prev" && cards[i].classList.contains("active")) {
+//       if (i - 1 >= 0) {
+//         toggleClass(cards[i], cards[i - 1]);
+//         break;
+//       } else {
+//         toggleClass(cards[i], cards[cards.length - 1]);
+//         break;
+//       }
+//     } else if (control === "next" && cards[i].classList.contains("active")) {
+//       console.log(i);
+//       if (i + 1 < cards.length) {
+//         // console.log(i);
+//         toggleClass(cards[i], cards[i + 1], "next");
+//         break;
+//       } else {
+//         toggleClass(cards[i], cards[0], "next");
+//         break;
+//       }
+//     } else {
+//       console.log("invalid control");
+//     }
+//   }
+// }
+
+// function toggleClass(present, next, className) {
+//   next.classList.add("active", className);
+//   present.classList.remove("active");
+// }
 
 function changeVal() {
   const val = input.value;
   const min = input.min;
   const max = input.max;
-  const thumbHalfWidth = 10;
-  const inputWidth = input.offsetWidth;
 
   const newVal = Math.ceil(((val - min) * 100) / (max - min));
   bubble.innerHTML = `${val} ${val == 1 ? "day" : "days"}`;
   // bubble.style.left = `calc(${newVal}% + (${10 - newVal * 0.3}px))`;
 }
+
 input.addEventListener("input", () => {
   changeVal();
 });
