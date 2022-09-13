01b5f0f26ec7a6c3d5071fcfd5c9d04c7ad35cb2
chubi-x <chubixax@protonmail.com>
2022-09-13 17:58:44 +0100

    feat: more motion && mobile responsive nav

--- script.js
+++ script.js
@@ -1,83 +1,133 @@
-const input = document.getElementById("pricing-slider");
-const bubble = document.getElementById("bubble");
+// ############################
+// LOADING STATE
+// ###########################
 
-const cards = document.querySelectorAll(".testimonial-card");
-const prevBtn = document.querySelector(".prev");
-const nextBtn = document.querySelector(".next");
+const loadingState = document.querySelector(".loading-state");
+const mainBody = document.querySelectorAll(".loading-state ~ *");
 
-let currSlide = 0;
-let maxSlides = cards.length - 1;
+document.querySelector("body").style.backgroundImage = "none";
+mainBody.forEach((el) => (el.style.display = "none"));
 
-function moveSlide(slides, currSlide) {
-  slides.forEach(
-    (slide, index) =>
+setTimeout(() => {
+  loadingState.style.display = "none";
+  mainBody.forEach((el) => {
+    el.nodeName !== "NAV" ?
+      (el.style.display = "block") :
+      (el.style.display = "flex");
+  });
+
+  document.querySelector("body").style.backgroundImage =
+    ' url("/images/blob.svg")';
+
+
+
+  // ###########################
+  // HAMBURGER MENU
+  // ##########################
+  const hamburger = document.querySelector('.hamburger-menu');
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('open')
+    document.querySelector('.mobile-menu').classList.toggle('menu-hidden')
+  })
+
+  // #############################
+  // TWINKLING STARS
+  // ############################
+
+  // credit: https://codepen.io/christinastep/pen/eXypvq?editors=0010
+  let i = 0;
+  while (i < 100) {
+    const el = document.createElement("img");
+    el.src = "/images/asterisk.svg";
+    el.alt = "star";
+    document.querySelector(".stars").appendChild(el);
+    i++;
+  }
+  const stars = document.querySelectorAll(".stars>img");
+
+  const services = document.querySelector(".services");
+  const starsContainer = document.querySelector(".stars");
+
+  starsContainer.style.width = `${services.offsetWidth / 1.2}px`;
+
+  // new star position
+  let newHeight, newWidth, height, width, time;
+
+  function newPosition() {
+    width = document.querySelector(".services").offsetWidth / 1.2;
+    height = document.querySelector(".services").offsetHeight / 2;
+    newHeight = Math.floor(Math.random() * height);
+    newWidth = Math.floor(Math.random() * width);
+    time = Math.floor(Math.random() * 1000) + 500;
+    return [newWidth, newHeight, time];
+  }
+
+  // random star locations
+  stars.forEach((img) => {
+    let animation = newPosition();
+    img.style.left = `${animation[0]}px`;
+    img.style.top = `${animation[1]}px`;
+    img.style.animationDuration = `${animation[2] / 200}s`;
+  });
+
+  // #########################
+  // TESTIMONIAL SLIDES
+  // #########################
+  const cards = document.querySelectorAll(".testimonial-card");
+  const prevBtn = document.querySelector(".prev");
+  const nextBtn = document.querySelector(".next");
+
+  let currSlide = 0;
+  let maxSlides = cards.length - 1;
+
+  function moveSlide(slides, currSlide) {
+    slides.forEach(
+      (slide, index) =>
       (slide.style.transform = `translateX(${(index - currSlide) * 100}%)`)
-  );
-}
+    );
+  }
+  prevBtn.addEventListener("click", () => {
+    if (currSlide === 0) {
+      currSlide = maxSlides;
+    } else currSlide--;
 
-prevBtn.addEventListener("click", () => {
-  if (currSlide === 0) {
-    currSlide = maxSlides;
-  } else currSlide--;
+    moveSlide(cards, currSlide);
+  });
+
+  nextBtn.addEventListener("click", () => {
+    if (currSlide === maxSlides) {
+      currSlide = 0;
+    } else currSlide++;
+    moveSlide(cards, currSlide);
+  });
+
+  setInterval(() => {
+    if (currSlide === maxSlides) {
+      currSlide = 0;
+    } else currSlide++;
+    moveSlide(cards, currSlide);
+  }, 3000)
 
   moveSlide(cards, currSlide);
-});
 
-nextBtn.addEventListener("click", () => {
-  if (currSlide === maxSlides) {
-    currSlide = 0;
-  } else currSlide++;
-  moveSlide(cards, currSlide);
-});
+  // ##########################
+  // INPUT CHANGE
+  // #########################
+  const input = document.getElementById("pricing-slider");
+  const bubble = document.getElementById("bubble");
 
-moveSlide(cards, currSlide);
+  function changeVal() {
+    const val = input.value;
+    const min = input.min;
+    const max = input.max;
 
-//#########################
-// OLD SLIDER LOGIC
-//#########################
+    const newVal = Math.ceil(((val - min) * 100) / (max - min));
+    bubble.innerHTML = `${val} ${val == 1 ? "day" : "days"}`;
+    // bubble.style.left = `calc(${newVal}% + (${10 - newVal * 0.3}px))`;
+  }
 
-// function slideControl(control, cards) {
-//   for (let i = 0; i < cards.length; i++) {
-//     if (control === "prev" && cards[i].classList.contains("active")) {
-//       if (i - 1 >= 0) {
-//         toggleClass(cards[i], cards[i - 1]);
-//         break;
-//       } else {
-//         toggleClass(cards[i], cards[cards.length - 1]);
-//         break;
-//       }
-//     } else if (control === "next" && cards[i].classList.contains("active")) {
-//       console.log(i);
-//       if (i + 1 < cards.length) {
-//         // console.log(i);
-//         toggleClass(cards[i], cards[i + 1], "next");
-//         break;
-//       } else {
-//         toggleClass(cards[i], cards[0], "next");
-//         break;
-//       }
-//     } else {
-//       console.log("invalid control");
-//     }
-//   }
-// }
-
-// function toggleClass(present, next, className) {
-//   next.classList.add("active", className);
-//   present.classList.remove("active");
-// }
-
-function changeVal() {
-  const val = input.value;
-  const min = input.min;
-  const max = input.max;
-
-  const newVal = Math.ceil(((val - min) * 100) / (max - min));
-  bubble.innerHTML = `${val} ${val == 1 ? "day" : "days"}`;
-  // bubble.style.left = `calc(${newVal}% + (${10 - newVal * 0.3}px))`;
-}
-
-input.addEventListener("input", () => {
+  input.addEventListener("input", () => {
+    changeVal();
+  });
   changeVal();
-});
-changeVal();
+}, 2500);
